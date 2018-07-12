22ae7bebfc6190d16b6f22a8ee8fe109ff746146
mynameisi <410145576@qq.com>
2018-07-12 21:18:50 +0800

    updated Movie docstring

--- media.py
+++ media.py
@@ -1,6 +1,12 @@
 class Movie():
 	def __init__(self,title,trailer_url,poster_url):
-	''''''
+		"""Form a Movie object
+
+	    Keyword arguments:
+	    title -- the title of the movie
+	    trailer_url -- the trailer url of the movie
+	    poster_url -- the url of a poster image of the movie
+	    """
 		self.title=title
 		self.trailer_youtube_url=trailer_url
 		self.poster_image_url=poster_url
